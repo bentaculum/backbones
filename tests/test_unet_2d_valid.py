5887edae03ee5fa2b67b118d27d32059f7888431
Benjamin Gallusser <bgallusser@googlemail.com>
2021-07-14 14:57:15 +0200

    Add Unet test

---
+++ tests/test_unet_2d_valid.py
@@ -0,0 +1,39 @@
+import pytest
+import numpy as np
+import torch
+from torch.nn import init
+from backbones import Unet2dValid
+
+
+@pytest.mark.parametrize("downsample_factors", [[(2, 4)], [(3, 3), (2, 2)]])
+def test_unet_2d_valid_output_ones(downsample_factors):
+
+    m = Unet2dValid(
+        in_channels=3,
+        initial_fmaps=4,
+        fmap_inc_factor=2,
+        downsample_factors=downsample_factors,
+        out_channels=3,
+        activation='ReLU',
+        batch_norm=False,
+    )
+    sizes = m.valid_input_sizes_seq(100)
+    x = torch.ones(1, 3, sizes[-2], sizes[-1])
+
+    for l in m.modules():
+        if isinstance(l, torch.nn.Conv2d):
+            init.constant_(l.weight,
+                           1 / (l.in_channels * np.prod(l.kernel_size)))
+            init.zeros_(l.bias)
+
+        if isinstance(l, torch.nn.ConvTranspose2d):
+            init.constant_(l.weight, 1 / l.in_channels)
+            init.zeros_(l.bias)
+
+    out = m(x)
+
+    assert torch.all(torch.isclose(out, torch.tensor(1.0))), f"{out=}"
+
+
+if __name__ == '__main__':
+    test_unet_2d_valid_unit_output()
